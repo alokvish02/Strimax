8e38339c09ce1cb422f09fa57e3e7bff99139b65
Alok <alokvishwakarma811@gmail.com>
2025-01-18 09:36:06 +0530

    final Update

---
+++ myimg/client/main.py
@@ -0,0 +1,15 @@
+import os
+
+# Directory containing the .heic files
+directory = r'C:\Users\Alok Vish\Desktop\Sterimax\myimg\client'
+
+# Get a list of all .heic files in the directory
+heic_files = [f for f in os.listdir(directory) if f.endswith('.png')]
+
+# Rename each file
+for i, filename in enumerate(heic_files, start=1):
+    new_filename = f'img_{i}.jpg'
+    old_file = os.path.join(directory, filename)
+    new_file = os.path.join(directory, new_filename)
+    os.rename(old_file, new_file)
+    print(f"Renamed {old_file} to {new_file}")
